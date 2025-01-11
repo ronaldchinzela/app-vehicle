fdee9d7804055c4ca2d667f28beaeb7f68a6e3f8
Ronald Chinchay <i201820124@cibertec.edu.pe>
2025-01-10 21:19:58 -0500

    Se agrega formato de precio y estilos en formulario

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-//TIEMPO DE 6 SEGUNDOS PARA QUE DESPAREZCA EL ALERT
+//TIEMPO DE 6 SEGUNDOS PARA QUE DESPAREZCA EL DIV DEL ALERT
 document.addEventListener("DOMContentLoaded", function () {
 
     const errorAlert = document.getElementById("error-alert");
@@ -24,5 +24,21 @@ document.addEventListener("DOMContentLoaded", function () {
             fadeOutAlert(successAlert);
         }, 5000);
     }
-
 });
+
+//FORMATO PARA LOS PRECIOS EN TODA LA TABLA
+function formatPrice(price) {
+    return 'US$ ' + price.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const priceCells = document.querySelectorAll('td[data-price]');
+    priceCells.forEach(function(cell) {
+        let price = parseFloat(cell.textContent.replace(/[^\d.-]/g, ''));
+        cell.textContent = formatPrice(price);
+    });
+});
+
